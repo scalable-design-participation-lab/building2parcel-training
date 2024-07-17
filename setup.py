d8f574bc5dd41cfdfb3b59664fe45daa3865d672
KzzD <davicekong@outlook.com>
2024-07-17 16:27:45 -0400

    Upload the module to pip library

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="parcel_building_mapper",
+    name="building2parcel-trainingdata",
     version="0.1.0",
     author="Dezeng Kong",
     author_email="kong.dez@northeastern.edu",
